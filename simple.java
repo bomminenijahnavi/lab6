f36dcb455fc315a289eee77a05e8ba1f937fa47a
bomminenijahnavi <jahnavichowdary536@gmail.com>
2026-04-01 12:02:39 +0530

    Add simple Java program to calculate sum

---
+++ simple.java
@@ -0,0 +1,8 @@
+public class simple {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 20;
+        int sum = firstNumber + secondNumber;
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
+    }
+}
